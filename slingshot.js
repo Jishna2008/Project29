89f547ec272826b3b9717b403e2adbb7c993a96d
JISHNA <77768080+Jishna2008@users.noreply.github.com>
2021-05-02 19:11:55 +0530

    Update slingshot.js

--- slingshot.js
+++ slingshot.js
@@ -18,8 +18,16 @@ display(){
     if(this.sling.bodyA){
     stroke("skyblue");
     strokeWeight(5);
-  var  point1=this.sling.bodyA.position;
-   var point2=this.pointB;
+    var  point1=this.sling.bodyA.position;
+    var point2=this.pointB;
     line(point1.x,point1.y,point2.x,point2.y);}
+    push()
+    textSize(20);
+    textFont("Times New Roman")
+    strokeWeight(5)
+    stroke("purple");
+     fill("white")
+    text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",50,50)
+    pop()
     }
 }
